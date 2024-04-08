7949a6a09b6546d042d42fac20110beba9e0a3eb
Fajobi Olusegun <segzyfaj1@gmail.com>
2024-04-08 08:58:33 +0100

    Added getPrice function

--- src/Fundme.sol
+++ src/Fundme.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
 contract Fundme {
 
     uint256 public MINIMUM_USD;
@@ -11,10 +14,12 @@ contract Fundme {
     }
 
     function getPrice() public view returns() {
-
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
     }
 
     function getConversionRate() public view returns() {
-        
+
     }
 }
